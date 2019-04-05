8ab9cffc9180a76e604e0a49f187bc777e1152c7
Amin <amin4udin@gmail.com>
2019-04-05 15:49:02 +0700

    update auto veryf waste

---
+++ auto_waste.js
@@ -0,0 +1,248 @@
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const delay = require("delay");
+const readline = require("readline-sync");
+const colors = require("./lib/colors");
+const fs = require("async-file");
+const fss = require("fs");
+const { URLSearchParams } = require("url");
+const moment = require("moment");
+const rp = require("request-promise");
+
+console.log("#####################");
+console.log("Panggil w Amin Tamvan");
+console.log("#####################");
+
+console.log("");
+console.log("");
+
+const file = readline.question("Masukan nama file result : ");
+
+console.log("");
+console.log("");
+
+const functionGetMessages = (email, domain) =>
+  new Promise((resolve, reject) => {
+    fetch(`https://waste.email/main.php?${email}@${domain}`, {
+      method: "get"
+    })
+      .then(res => res.text())
+      .then(body => {
+        try {
+          const $ = cheerio.load(body);
+          const src = $(".button").attr("href");
+          resolve(src);
+        } catch (e) {
+          console.log(body);
+        }
+      })
+      .catch(err =>
+        console.log(
+          "[" +
+            " " +
+            moment().format("HH:mm:ss") +
+            " " +
+            "]" +
+            " " +
+            "Ada masalah sssSssstt..."
+        )
+      );
+  });
+
+const functionVerification = (email, token) =>
+  new Promise((resolve, reject) => {
+    const params = new URLSearchParams();
+    params.append("email", email);
+    params.append("verification_code", token);
+
+    fetch("https://api.bigtoken.com/signup/email-verification", {
+      method: "POST",
+      body: params,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded ",
+        "Content-Length": 387,
+        Host: "api.bigtoken.com",
+        Connection: "Keep-Alive",
+        "Accept-Encoding": "gzip ",
+        "User-Agent": "okhttp/3.14.0"
+      }
+    })
+      .then(res => res.text())
+      .then(text => {
+        resolve(text);
+      })
+      .catch(err => reject(err));
+  });
+
+const functionGetLocation = domain =>
+  new Promise((resolve, reject) => {
+    // rp({
+    //   uri: "https://bigtoken.page.link/og5e4wEN3Difa11i7",
+    //   method: "GET",
+    //   followAllRedirects: true
+
+    // headers: {
+    //   accept:
+    //     "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+    //   "accept-encoding": "gzip, deflate, br",
+    //   "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+    //   "upgrade-insecure-requests": 1,
+    //   "user-agent":
+    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
+    // }
+    // }).then(function(response) {
+    //   resolve(response.headers);
+    // });
+
+    const userAgent =
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36";
+    const url = `${domain}`;
+
+    const _include_headers = function(body, response, resolveWithFullResponse) {
+      return {
+        headers: response.headers,
+        data: body,
+        finalUrl: response.request.uri.href // contains final URL
+      };
+    };
+
+    const options = {
+      uri: url,
+      followAllRedirects: true,
+      method: "get",
+      gzip: true,
+      transform: _include_headers,
+      headers: {
+        "User-Agent": userAgent
+      }
+    };
+
+    const p1 = rp(options).then((response, error, html) => {
+      resolve(response.finalUrl);
+    });
+  });
+
+(async () => {
+  console.log(
+    "[" + " " + moment().format("HH:mm:ss") + " " + "]" + " " + "MEMULAI ...."
+  );
+  await fss.readFile(file, async function(err, data) {
+    if (err) throw err;
+    const array = data
+      .toString()
+      .replace(/\r\n|\r|\n/g, " ")
+      .split(" ");
+
+    const regEX = /[.-\w]+@[\w\-]{3,}(.\w{2,})+/;
+    const test = array.map(ury => {
+      return ury.match(regEX);
+    });
+
+    test.map(async eml => {
+      if (eml !== null) {
+        const regMail = /(?<=@)[^.]+.([^.]+)$/m;
+
+        const uname = eml[0].split("@")[0];
+        const domain = eml[0].match(regMail)[0];
+
+        const email = uname + "@" + domain;
+
+        await delay(120000);
+        const message = await functionGetMessages(uname, domain);
+        console.log(message);
+
+        if (message === undefined) {
+          console.log(
+            "[" +
+              " " +
+              moment().format("HH:mm:ss") +
+              " " +
+              "]" +
+              " " +
+              "TOKEN EXPIRED / BELUM ADA EMAIL....."
+          );
+          console.log("");
+          console.log("");
+        } else {
+          if (message == 44) {
+            const getLocation = await functionGetLocation(message);
+
+            const regex = await new RegExp(/\?(?:code)\=([\S\s]*?)\&/);
+            const regexEm = await new RegExp(/[.\w]+@[\w\-]{3,}(.\w{2,})+/);
+            const resGex = await regex.exec(getLocation);
+            const resGexEm = await regexEm.exec(getLocation);
+            await delay(120000);
+            const veryf = await functionVerification(resGexEm[0], resGex[1]);
+            // const msg = JSON.parse(veryf).error.status;
+
+            if (JSON.parse(veryf).hasOwnProperty("error")) {
+              console.log(
+                "[" +
+                  " " +
+                  moment().format("HH:mm:ss") +
+                  " " +
+                  "]" +
+                  " " +
+                  `Email : ${resGexEm[0]}` +
+                  " " +
+                  "Token Expired"
+              );
+            } else {
+              console.log(
+                "[" +
+                  " " +
+                  moment().format("HH:mm:ss") +
+                  " " +
+                  "]" +
+                  " " +
+                  `Email : ${resGexEm[0]}` +
+                  " " +
+                  "Veryf Sukses"
+              );
+            }
+          } else {
+            const decodeURL = await decodeURIComponent(message);
+
+            const regex = await new RegExp(/\?(?:code)\=([\S\s]*?)\&/);
+            const regexEm = await new RegExp(
+              /([\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/
+            );
+            const resGex = await regex.exec(decodeURL);
+            const resGexEm = await regexEm.exec(decodeURL);
+
+            await delay(DelaY);
+            const veryf = await functionVerification(resGexEm[0], resGex[1]);
+            // const msg = JSON.parse(veryf).error.status;
+
+            if (JSON.parse(veryf).hasOwnProperty("error")) {
+              console.log(
+                "[" +
+                  " " +
+                  moment().format("HH:mm:ss") +
+                  " " +
+                  "]" +
+                  " " +
+                  `Email : ${resGexEm[0]}` +
+                  " " +
+                  "Token Expired"
+              );
+            } else {
+              console.log(
+                "[" +
+                  " " +
+                  moment().format("HH:mm:ss") +
+                  " " +
+                  "]" +
+                  " " +
+                  `Email : ${resGexEm[0]}` +
+                  " " +
+                  "Veryf Sukses"
+              );
+            }
+          }
+        }
+      }
+    });
+  });
+})();
